a0a7b592c472b9dd592629b5d8997de75ab68ed9
vampiressa <yuliadervenkova@gmail.com>
2019-02-01 15:58:14 +0300

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 import {SpinnerService} from '@core/services/spinner.service';
+import { UIRouter } from '@uirouter/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 
   public showLoader: boolean;
 
-  constructor(private spinnerService: SpinnerService) {
-
+  constructor(private spinnerService: SpinnerService, private router: UIRouter) {
   }
 
   ngOnInit() {
     this.spinnerService.status.subscribe((val: boolean) => {
       this.showLoader = val;
     });
+    this.router.stateService.go('login');
   }
 
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {OauthService} from '@core/services/oauth.service';
 
@@ -23,3 +24,48 @@ export class LoginComponent implements OnInit {
   }
 
 }
+=======
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {OauthService} from '@core/services/oauth.service';
+import { UIRouter } from '@uirouter/core';
+import { ShowHideInput } from '../../core/directives/show-hide-input';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  @ViewChild('form') private form;
+  @ViewChild(ShowHideInput) input: ShowHideInput;
+  show = false;
+
+  constructor(public authService: OauthService, private router: UIRouter) {
+  }
+
+  ngOnInit() {
+  }
+
+  showPassword() {
+    this.show = !this.show;
+    if (this.show) {
+      this.input.changeType("text");
+    } else {
+      this.input.changeType("password");
+    }
+  }
+
+  public login() {
+    if (this.form.valid) {
+      this.authService.login().subscribe(
+        (res)=> {
+          this.authService.setTokenAndSetUserData(res, true);
+          this.router.stateService.go('alerts');
+        }
+      );
+    }
+  }
+
+}
+>>>>>>> 028fe02... login
